b4d58329c6c8be68840f0d315390a4695c3bf0d9
Alexa Moradi <alexamoradi@alexamoradismbp.attlocal.net>
2018-07-22 23:59:11 -0500

    add off stats and player queries

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # This is a spike.
-
-https://www.armchairanalysis.com/apidoc/#api-players-GetActivePlayers
-https://armchairanalysis.com/api/1.0/test/players
--- schema/schema.js
+++ schema/schema.js
@@ -1,15 +1,30 @@
 const graphql = require('graphql');
+const axios = require('axios');
+const apiBase = 'https://armchairanalysis.com/api/1.0/test';
 const { 
     GraphQLInt,
     GraphQLObjectType,
     GraphQLSchema,
-    GraphQLString
+    GraphQLString,
+    GraphQLList
 } = graphql;
+
+const OffensiveStats = new GraphQLObjectType({
+    name: 'OffensiveStats', 
+    fields: {
+        player: { type: GraphQLString },
+        game: { type: GraphQLInt }, 
+        year: { type: GraphQLInt },
+        pa: { type: GraphQLInt },
+        rec: { type: GraphQLInt },
+        fp: { type: GraphQLString }
+    }
+});
 
 const Player = new GraphQLObjectType({
     name: 'Player',
     fields: {
-        id: { type: GraphQLString}, 
+        player: { type: GraphQLString}, 
         fName: { type: GraphQLString},
         lName: { type: GraphQLString},
         dob: { type: GraphQLString},
@@ -18,6 +33,16 @@ const Player = new GraphQLObjectType({
         team: { type: GraphQLString},
         height: { type: GraphQLInt},
         weight: { type: GraphQLInt},
+        offensiveStats: {
+            type: GraphQLList(OffensiveStats),
+            resolve(parentValue, args) {
+                return axios.get(`${apiBase}/player/${parentValue.player}/offense`)
+                    .then(({ data }) => {
+                        
+                        return data.data;
+                    });
+            }
+        }
     }
 });
 
@@ -26,9 +51,25 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         player: {
             type: Player, 
-            args: { id: { type: GraphQLString }},
+            args: { id: { type: GraphQLString }, fName: { type: GraphQLString }, lName: { type: GraphQLString }},
             resolve(parentValue, args) {
+                const { status, id, fName, lName } = args;
+                const token = id ? id : `${fName}_${lName}`;
 
+                return axios.get(`${apiBase}/player/${token}`)
+                    .then(({ data: { data }}) => {
+                        return Array.isArray(data) ? data[0] : data;
+                    });
+            }
+        },
+        players: {
+            type: GraphQLList(Player), 
+            args: { status: { type: GraphQLString }, count: { type: GraphQLString }, start: { type: GraphQLString }},
+            resolve(parentValue, args) {
+                const { status, count, start } = args;
+
+                return axios.get(`${apiBase}/players?status=${status}`)
+                    .then(({ data: { data }}) => data);
             }
         }
     }
